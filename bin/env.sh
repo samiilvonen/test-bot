eac5e908433dfe253afa26db746ec8271b95b6e4
Martti Louhivuori <martti.louhivuori@csc.fi>
2017-07-07 17:02:29 +0300

    add command to PATH and fix dirname under Slurm

--- bin/env.sh
+++ bin/env.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-export TESTBOT=$(dirname ${BASH_SOURCE[@]} | sed -e 's|/bin$|/|')
+export TESTBOT=$(dirname ${BASH_SOURCE[0]} | sed -e 's|/bin$|/|')
 
 if [ -e $TESTBOT/lib ]
 then
     [[ -z "$PYTHONPATH" ]] && PYTHONPATH=$TESTBOT/lib \
         || PYTHONPATH=$TESTBOT/lib:$PYTHONPATH
     export PYTHONPATH
+    [[ -z "$PATH" ]] && PATH=$TESTBOT/bin || PATH=$TESTBOT/bin:$PATH
+    export PATH
 elif [ -e $TESTBOT/testbot ]
 then
     [[ -z "$PYTHONPATH" ]] && PYTHONPATH=$TESTBOT \
         || PYTHONPATH=$TESTBOT:$PYTHONPATH
     export PYTHONPATH
+    [[ -z "$PATH" ]] && PATH=$TESTBOT/bin || PATH=$TESTBOT/bin:$PATH
+    export PATH
 else
     echo "Error: No known location for testbot."
     echo "  Either run directly from the git repository"
